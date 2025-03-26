7fdcd662b17f697d23bb50818c2f3e8ddc7435d9
hungcucu <tranngochungb046@gmail.com>
2025-03-26 10:36:42 +0700

    updated utils files

--- utils/DOpt.java
+++ utils/DOpt.java
@@ -1,8 +1,36 @@
 package utils;
-import java.lang.annotation.*;
 
-@Retention(RetentionPolicy.RUNTIME) // Available at runtime
-@Target(ElementType.METHOD) // Can only be used on methods
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ METHOD, CONSTRUCTOR })
+/**
+ * @overview 
+ *  Annotate a method or constructor as a domain operation.
+ *  
+ * @author dmle
+ *
+ * @version 2017 
+ */
 public @interface DOpt {
-    OptType type(); // Requires an OptType value
+
+  /**
+   *  The operation type 
+   */
+  OptType type();
+
+  /**
+   *  whether or not the behaviour of the associated operation is optional for the 
+   *  owner class to function.
+   *  <br>Default: <tt>false</tt>   
+   */
+  boolean optional() default false;
+
 }
